c76145fd38f6a8d8b9fbd7365df9c4b5d0ee4db4
emiliocantumtz <89506135+emiliocantumtz@users.noreply.github.com>
2023-05-22 17:58:02 -0600

    Create DocTour.py

---
+++ DocTour.py
@@ -0,0 +1,37 @@
+from _future_ import annotations
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+def sidebar():
+    # st.title("O")
+    st.markdown("<h1 style='text-align: center; color: #7D3C98 ;'> Rearreanged</h1>", unsafe_allow_html=True)
+    with st.sidebar:
+        st.header("Encabezado")
+        st.subheader("Subencabezado")
+        text = st.text_area("Filtrar ")
+
+        button0 = st.button("Golf")
+        if button0:
+            st.write(text)
+ 
+def header():
+  st.header("TRC")
+  col1, col2 = st.columns(2)
+  
+  with col1:
+    button1 = st.button("TRC?")
+    if button1:
+        st.write("Si")
+    elif not button1:
+        st.write("Golf")
+        
+  with col2:
+    button2 = st.button("TRC")
+    if button2:
+        st.write("No")
+    elif not button1:
+        st.write("Golf'nt")
+          
+sidebar()
+header()
