1db29abd5d4c033269eef0296ed5eaa4b1bac0f7
emiliocantumtz <89506135+emiliocantumtz@users.noreply.github.com>
2023-05-22 18:38:46 -0600

    Update DocTour.py

--- DocTour.py
+++ DocTour.py
@@ -5,14 +5,6 @@ import numpy as np
 def sidebar():
     # st.title("O")
     st.markdown("<h1 style='text-align: center; color: #7D3C98 ;'> DocTour</h1>", unsafe_allow_html=True)
-    with st.sidebar:
-        st.header("Encabezado")
-        st.subheader("Subencabezado")
-        text = st.text_area("Filtrar ")
-
-        button0 = st.button("Golf")
-        if button0:
-            st.write(text)
  
 def header():
   st.header("Variables Críticas")
