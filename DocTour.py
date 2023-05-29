f334bead10ae0b32b3519740e133b4a531dd1075
emiliocantumtz <89506135+emiliocantumtz@users.noreply.github.com>
2023-05-29 17:00:44 -0600

    Update DocTour.py

--- DocTour.py
+++ DocTour.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from streamlit_option_menu import option _menu
+from streamlit_option_menu import option_menu
 
 def sidebar():
     # st.title("O")
