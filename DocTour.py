fd56e7f8e0d6f749ff2cba5da5d340134578ed2f
emiliocantumtz <89506135+emiliocantumtz@users.noreply.github.com>
2023-05-31 08:05:24 -0600

    Update DocTour.py

--- DocTour.py
+++ DocTour.py
@@ -3,6 +3,8 @@ from streamlit_option_menu import option_menu
 import pandas as pd
 import numpy as np
 import altair as alt
+import streamlit as st
+import matplotlib.pyplot as plt
 
 def sidebar():
     # st.title("O")
@@ -29,7 +31,7 @@ def buttons():
     col3.metric("Ventas membresía platino Mayo 2023", "2", "5%")
   
 def chart():
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3, col4 = st.columns(4)
             
     with col1:
             source = pd.DataFrame({
@@ -64,6 +66,19 @@ def chart():
                 x = "Leads",
             )
             st.altair_chart(line_chart, use_container_width=True)
+            
+     with col4:
+        # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+            labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+            sizes = [15, 30, 45, 10]
+            explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+            fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+st.pyplot(fig1)
           
 sidebar()
 buttons()
