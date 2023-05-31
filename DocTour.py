285cd331575f4e8ce64804204597f92861925a38
emiliocantumtz <89506135+emiliocantumtz@users.noreply.github.com>
2023-05-30 23:45:42 -0600

    Update DocTour.py

--- DocTour.py
+++ DocTour.py
@@ -15,63 +15,63 @@ def sidebar():
         orientation = "horizontal",
     )
     
-    if select == "Home":
+    if select == "Resumen mensual":
+        def buttons():
+            st.header("DocTour")
+            col1, col2, col3 = st.columns(3)
+        
+            with col1:
+                col1.metric("Ingresos por membresía Mayo 2023", "120K", "20%")
+        
+            with col2:
+                col2.metric("Costos de operación Mayo 2023", "100K", "-4.2%")
+
+            with col3:
+                col3.metric("Utilidad/Pérdida Mayo 2023", "20K", "20%")
+  
+        def chart():
+            col1, col2, col3 = st.columns(3)
+    
+            with col1:
+                    source = pd.DataFrame({
+                        "Ingresos":[ 15, 14, 9, 24, 11, 15, 15, 18, 14, 15, 13, 17],
+                        "Mes":["ENE", "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AGO", "SEPT", "OCT", "NOV", "DIC"]
+                    })
+                
+                    line_chart = alt.Chart(source).mark_line().encode(
+                        y = "Ingresos",
+                        x = "Mes",
+                    )
+                    st.altair_chart(line_chart, use_container_width=True)
+            
+            with col2:
+                    source = pd.DataFrame({
+                        "Membresias":[ 15, 14, 9, 24, 11, 15, 15, 18, 14, 15, 13, 17],
+                        "Mes":["ENE", "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AGO", "SEPT", "OCT", "NOV", "DIC"]
+                    })
+                
+                    bar_chart = alt.Chart(source).mark_bar().encode(
+                        y = "Membresias",
+                        x = "Mes",
+                    )
+                    st.altair_chart(bar_chart, use_container_width=True)
+            with col3:
+                    source = pd.DataFrame({
+                        "Membresias":[ 180, 153, 197, 76],
+                        "Año":["2020", "2021", "2022", "2023"]
+                    })
+                
+                    bar_chart = alt.Chart(source).mark_bar().encode(
+                        y = "Membresias",
+                        x = "Año",
+                    )
+                    st.altair_chart(bar_chart, use_container_width=True)
+                    
+    elif select == "Detalle de ingresos mensuales":
         st.title(f"You have selected {select}")
-    elif select == "Projects":
-        st.title(f"You have selected {select}")
-    elif select == "Contact":
+    elif select == "Detalle de gastos mensuales":
         st.title(f"You have selected {select}") 
 
-def buttons():
-  st.header("DocTour")
-  col1, col2, col3 = st.columns(3)
-        
-  with col1:
-    col1.metric("Ingresos por membresía Mayo 2023", "120K", "20%")
-        
-  with col2:
-    col2.metric("Costos de operación Mayo 2023", "100K", "-4.2%")
-
-  with col3:
-    col3.metric("Utilidad/Pérdida Mayo 2023", "20K", "20%")
-  
-def chart():
-    col1, col2, col3 = st.columns(3)
-    
-    with col1:
-            source = pd.DataFrame({
-                "Ingresos":[ 15, 14, 9, 24, 11, 15, 15, 18, 14, 15, 13, 17],
-                "Mes":["ENE", "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AGO", "SEPT", "OCT", "NOV", "DIC"]
-            })
-                
-            line_chart = alt.Chart(source).mark_line().encode(
-                y = "Ingresos",
-                x = "Mes",
-            )
-            st.altair_chart(line_chart, use_container_width=True)
-            
-    with col2:
-            source = pd.DataFrame({
-                "Membresias":[ 15, 14, 9, 24, 11, 15, 15, 18, 14, 15, 13, 17],
-                "Mes":["ENE", "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AGO", "SEPT", "OCT", "NOV", "DIC"]
-            })
-                
-            bar_chart = alt.Chart(source).mark_bar().encode(
-                y = "Membresias",
-                x = "Mes",
-            )
-            st.altair_chart(bar_chart, use_container_width=True)
-    with col3:
-            source = pd.DataFrame({
-                "Membresias":[ 180, 153, 197, 76],
-                "Año":["2020", "2021", "2022", "2023"]
-            })
-                
-            bar_chart = alt.Chart(source).mark_bar().encode(
-                y = "Membresias",
-                x = "Año",
-            )
-            st.altair_chart(bar_chart, use_container_width=True)
           
 sidebar()
 buttons()
